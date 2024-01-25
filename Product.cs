e724edc3deec7d8d0c1c5e8b458879649e704ef9
Porstoyashka <155156452+Porstoyashka@users.noreply.github.com>
2024-01-25 16:49:43 +0200

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineStoreProjectOOP;
+
+public class Product
+{
+    public int ID;
+    public Category Type;
+    public string? Name;
+    public double Price;
+
+    public Product(int id, Category type, string? name, double price)
+    {
+        ID = id;
+        Type = type;
+        Name = name;
+        Price = price;
+    }
+}
