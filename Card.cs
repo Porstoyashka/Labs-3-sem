37829ca8f7670d055a11924ebb3c81af8d697fff
Porstoyashka <155156452+Porstoyashka@users.noreply.github.com>
2024-01-25 16:46:23 +0200

    Create Card.cs

---
+++ Card.cs
@@ -0,0 +1,28 @@
+﻿namespace OnlineStoreProjectOOP;
+
+public class Card
+{
+    private int Number;
+    private double Sum;
+
+    public Card(int number, int sum)
+    {
+        Number = number;
+        Sum = sum;
+    }
+
+    public double GetSum()
+    {
+        return Sum;
+    }
+
+    public void SetSum(double Sum)
+    {
+        this.Sum = Sum;
+    }
+
+    public int GetNumber()
+    {
+        return Number;
+    }
+}
