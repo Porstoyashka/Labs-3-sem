55bea0860c71ae706283092b5daff94df583eb44
Porstoyashka <155156452+Porstoyashka@users.noreply.github.com>
2024-01-25 16:47:34 +0200

    Create DataBase.cs

---
+++ DataBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace OnlineStoreProjectOOP;
+
+public abstract class DataBase
+{
+    public List<Product>? ProductsList { get; set; }
+    public List<User> UserLists;
+
+    public DataBase()
+    {
+        ProductsList = new List<Product>();
+        UserLists = new List<User>
+        {
+            new User("1", GetHash("1"), new Card(1001, 10000), false),
+            new User("Ivan", GetHash("2345"), new Card(1002, 13000), true),
+            new User("Artem", GetHash("1234"), new Card(1003, 8000), false),
+        };
+    }
+
+    public abstract void Show(Category product);
+    public abstract Product SetProduct(int id);
+    public abstract void RemoveProductWithProductsList(Product product);
+    
+    public static string GetHash(string? password)
+    {
+        using (var hash = SHA1.Create())
+        {
+            return string.Concat(hash.ComputeHash(Encoding.UTF8.GetBytes(password))
+                .Select(x => x.ToString("X2")));
+        }
+    }
+}
